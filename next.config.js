2872910c86f9baec5a6c681e147dc605e26ab58f
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-03-28 08:35:56 +0530

    test

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
       ? process.env.NEXT_PUBLIC_BASEPATH
       : "",
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "https://www.intself.com",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
