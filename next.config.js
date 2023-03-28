1c9c348a4c0e37a20fa34631b29bbec2dc1fc310
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-03-28 08:17:27 +0530

    k

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   basePath:
     process.env.NODE_ENV === "production"
       ? process.env.NEXT_PUBLIC_BASEPATH
       : "",
-  // images: {
-  //   loader: 'akamai',
-  //   path: process.env.NEXT_PUBLIC_URL,
-  // },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL || "https://www.intself.com",
-    GOOGLE_ID: process.env.GOOGLE_ID,
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    GOOGLE_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
 };
 
