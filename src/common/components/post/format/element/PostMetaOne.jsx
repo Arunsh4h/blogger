99e1c804ce8a454441196b0fe59684bc9496f088
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-04-11 03:15:28 +0530

    hgl

--- src/common/components/post/format/element/PostMetaOne.jsx
+++ src/common/components/post/format/element/PostMetaOne.jsx
@@ -1,129 +1,132 @@
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { slugify } from "../../../../utils";
-import { Helmet } from "react-helmet";
 
 const PostMetaOne = ({ metaData }) => {
-  const title = metaData.title;
-  const description = metaData.excerpt;
-  const image = metaData.featureImg;
-  const url = `https://intself.com/post/${slugify(title)}`;
-  const authorName = metaData.author_name;
-  const authorImage = metaData.author_img;
-  const authorSocial = metaData.author_social.map((social) => ({
-    "@type": "SocialMediaPosting",
-    name: social.name,
-    url: social.url,
-  }));
-
   return (
-    <>
-      <Helmet>
-        <script type="application/ld+json">
-          {`{
-            "@context": "https://schema.org",
-            "@type": "Article",
-            "mainEntityOfPage": {
-              "@type": "WebPage",
-              "@id": "${url}"
-            },
-            "headline": "${title}",
-            "description": "${description}",
-            "image": "${image}",
-            "author": {
-              "@type": "Person",
-              "name": "${authorName}",
-              "image": "${authorImage}",
-              "sameAs": ${JSON.stringify(authorSocial)}
-            },
-            "datePublished": "${metaData.date}",
-            "dateModified": "${metaData.modified_date}"
-          }`}
-        </script>
-      </Helmet>
-      <div className="banner banner-single-post post-formate post-standard alignwide">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              {/* Start Single Slide  */}
-              <div className="content-block">
-                {/* Start Post Thumbnail  */}
-                <div className="post-thumbnail">
-                  <Image
-                    src={metaData.featureImg}
-                    alt={metaData.title}
-                    height={720}
-                    width={1440}
-                  />
-                </div>
-                {/* End Post Thumbnail  */}
-                {/* Start Post Content  */}
-                <div className="post-content">
-                  <div className="post-cat">
-                    <div className="post-cat-list">
-                      <Link href={`/category/${slugify(metaData.cate)}`}>
-                        <a className="hover-flip-item-wrapper">
-                          <span className="hover-flip-item">
-                            <span data-text={metaData.cate}>
-                              {metaData.cate}
-                            </span>
-                          </span>
-                        </a>
-                      </Link>
-                    </div>
+    <article
+      className="banner banner-single-post post-formate post-standard alignwide"
+      itemType="http://schema.org/Article"
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            {/* Start Single Slide  */}
+            <div className="content-block">
+              {/* Start Post Thumbnail  */}
+              <figure
+                className="post-thumbnail"
+                itemProp="image"
+                itemScope
+                itemType="http://schema.org/ImageObject"
+              >
+                <Image
+                  src={metaData.featureImg}
+                  alt={metaData.title}
+                  height={720}
+                  width={1440}
+                  itemProp="contentUrl"
+                />
+                <meta itemProp="width" content="1440" />
+                <meta itemProp="height" content="720" />
+              </figure>
+              {/* End Post Thumbnail  */}
+              {/* Start Post Content  */}
+              <div
+                className="post-content"
+                itemScope
+                itemType="http://schema.org/Text"
+              >
+                <div className="post-cat">
+                  <div className="post-cat-list">
+                    <Link href={`/category/${slugify(metaData.cate)}`}>
+                      <a className="hover-flip-item-wrapper">
+                        <span className="hover-flip-item" itemProp="about">
+                          <span data-text={metaData.cate}>{metaData.cate}</span>
+                        </span>
+                      </a>
+                    </Link>
                   </div>
-                  <h1 className="title">{metaData.title}</h1>
-                  {/* Post Meta  */}
-                  <div className="post-meta-wrapper">
-                    <div className="post-meta">
-                      <div className="post-author-avatar border-rounded">
-                        <Image
-                          src={metaData.author_img}
-                          alt={metaData.author_name}
-                          height={50}
-                          width={50}
-                        />
-                      </div>
-                      <div className="content">
-                        <h6 className="post-author-name">
-                          <Link
-                            href={`/author/${slugify(metaData.author_name)}`}
-                          >
-                            <a className="hover-flip-item-wrapper">
-                              <span className="hover-flip-item">
-                                <span data-text={metaData.author_name}>
-                                  {metaData.author_name}
-                                </span>
+                </div>
+                <h1 className="title" itemProp="headline">
+                  {metaData.title}
+                </h1>
+                {/* Post Meta  */}
+                <div
+                  className="post-meta-wrapper"
+                  itemScope
+                  itemType="http://schema.org/Person"
+                >
+                  <div className="post-meta">
+                    <div
+                      className="post-author-avatar border-rounded"
+                      itemProp="image"
+                      itemScope
+                      itemType="http://schema.org/ImageObject"
+                    >
+                      <Image
+                        src={metaData.author_img}
+                        alt={metaData.author_name}
+                        height={50}
+                        width={50}
+                        itemProp="contentUrl"
+                      />
+                      <meta itemProp="width" content="50" />
+                      <meta itemProp="height" content="50" />
+                    </div>
+                    <div className="content">
+                      <h6 className="post-author-name" itemProp="name">
+                        <Link href={`/author/${slugify(metaData.author_name)}`}>
+                          <a className="hover-flip-item-wrapper">
+                            <span className="hover-flip-item">
+                              <span data-text={metaData.author_name}>
+                                {metaData.author_name}
                               </span>
-                            </a>
-                          </Link>
-                        </h6>
-                        <ul className="post-meta-list">
-                          <li>{metaData.date}</li>
-                          <li>{metaData.post_views}</li>
-                        </ul>
-                      </div>
-                    </div>
-                    <ul className="social-share-transparent justify-content-end">
-                      {metaData.author_social.map((social) => (
-                        <li key={social.url}>
-                          <a href={social.url}>
-                            <i className={social.icon} />
+                            </span>
                           </a>
+                        </Link>
+                      </h6>
+                      <ul className="post-meta-list">
+                        <li itemProp="datePublished">{metaData.date}</li>
+                        <li
+                          itemProp="interactionStatistic"
+                          itemScope
+                          itemType="http://schema.org/InteractionCounter"
+                        >
+                          <meta
+                            itemProp="interactionType"
+                            content="http://schema.org/ViewAction"
+                          />
+                          <meta
+                            itemProp="userInteractionCount"
+                            content={metaData.post_views}
+                          />
+                          {metaData.post_views} views
                         </li>
-                      ))}
-                    </ul>
+                      </ul>
+                    </div>
                   </div>
+                  <ul
+                    className="social-share-transparent justify-content-end"
+                    itemProp="sameAs"
+                  >
+                    {metaData.author_social.map((social) => (
+                      <li key={social.url}>
+                        <a href={social.url}>
+                          <i className={social.icon} />
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
-                {/* End Post Content  */}
               </div>
-              {/* End Single Slide  */}
+              {/* End Post Content  */}
             </div>
+            {/* End Single Slide  */}
           </div>
         </div>
       </div>
-    </>
+    </article>
   );
 };
 
