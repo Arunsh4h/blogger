6e9a7cad32e897ac4684ea1fc232079f64471e38
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-04-11 01:07:20 +0530

    gghh

--- src/common/components/post/format/element/PostMetaOne.jsx
+++ src/common/components/post/format/element/PostMetaOne.jsx
@@ -8,8 +8,7 @@ const PostMetaOne = ({ metaData }) => {
   const title = metaData.title;
   const description = metaData.excerpt;
   const image = metaData.featureImg;
-  const currentUrl = window.location.href;
-  const url = `${currentUrl}/post/${slugify(title)}`;
+  const url = `https://example.com/post/${slugify(title)}`;
   const authorName = metaData.author_name;
   const authorImage = metaData.author_img;
   const authorSocial = metaData.author_social.map((social) => ({
