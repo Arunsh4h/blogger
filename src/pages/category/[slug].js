0be3b14d663d20d37a738cd30c628462249e5acc
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-04-11 06:11:39 +0530

    hgf

--- src/pages/category/[slug].js
+++ src/pages/category/[slug].js
@@ -8,7 +8,6 @@ import HeaderOne from "../../common/elements/header/HeaderOne";
 import SidebarOne from "../../common/components/sidebar/SidebarOne";
 import { slugify } from "../../common/utils";
 import React, { useState } from "react"; // Import useState hook from React
-import css from "styled-jsx/css";
 
 const PostCategory = ({ postData, allPosts }) => {
   const [numToShow, setNumToShow] = useState(5); // State variable to keep track of number of posts to show
@@ -17,36 +16,6 @@ const PostCategory = ({ postData, allPosts }) => {
     // Function to handle "Load More" button click
     setNumToShow(numToShow + 3); // Update state variable to increase number of posts to show by 3
   };
-  const styles = css`
-    /* rocket-ajax container */
-    .rocket-ajax-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-    /* rocket-ajax */
-    .rocket-ajax {
-      display: inline-block;
-      padding: 12px 24px;
-      font-size: 16px;
-      font-weight: bold;
-      text-align: center;
-      text-transform: uppercase;
-      color: #ffffff;
-      background-color: #007bff;
-      border: 9px solid #c2e2ff;
-      border-radius: 10px;
-      cursor: pointer;
-      transition: background-color 0.3s ease;
-    }
-
-    /* rocket-ajax hover effect */
-    .rocket-ajax:hover {
-      background-color: #ff6767;
-      border: 9px solid #ffe6c3;
-    }
-  `;
 
   return (
     <>
@@ -59,10 +28,26 @@ const PostCategory = ({ postData, allPosts }) => {
             <div className="col-lg-8 col-xl-8">
               <PostLayoutTwo dataPost={postData} show={numToShow} />{" "}
               {/* Pass updated state variable as number of posts to show */}
-              <button className="rocket-ajax" onClick={handleLoadMore}>
+              <button
+                className="rocket-ajax"
+                onClick={handleLoadMore}
+                style={{
+                  display: "inline-block",
+                  padding: "12px 24px",
+                  fontSize: "16px",
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  textTransform: "uppercase",
+                  color: "#ffffff",
+                  backgroundColor: "#007bff",
+                  border: "9px solid #c2e2ff",
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                  transition: "background-color 0.3s ease",
+                }}
+              >
                 Load More
               </button>{" "}
-              {/* Add "Load More" button with click event handler */}
             </div>
             <div className="col-lg-4 col-xl-4 mt_md--40 mt_sm--40">
               <SidebarOne dataPost={allPosts} />
@@ -72,7 +57,6 @@ const PostCategory = ({ postData, allPosts }) => {
       </div>
       <InstagramOne parentClass="bg-color-grey" />
       <FooterOne />
-      <style jsx>{styles}</style>
     </>
   );
 };
