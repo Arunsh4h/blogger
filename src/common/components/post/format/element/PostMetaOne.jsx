26c445d4e3afa60224d7e0fa7367f1e11dda8df2
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-04-24 06:40:49 +0530

    gf

--- src/common/components/post/format/element/PostMetaOne.jsx
+++ src/common/components/post/format/element/PostMetaOne.jsx
@@ -40,15 +40,15 @@ const PostMetaOne = ({ metaData, content }) => {
                 itemType="http://schema.org/Text"
               >
                 <div className="post-cat">
-                  <div
-                    style={{
-                      boxShadow: "0px 0px 0px 20px rgb(205 229 267)",
-                      fontFamily: "'Josefin Sans', sans-serif",
-                    }}
-                    className="post-cat-list"
-                  >
+                  <div className="post-cat-list">
                     <Link href={`/category/${slugify(metaData.cate)}`}>
-                      <a className="hover-flip-item-wrapper">
+                      <a
+                        style={{
+                          boxShadow: "0px 0px 0px 20px rgb(205 229 267)",
+                          fontFamily: "'Josefin Sans', sans-serif",
+                        }}
+                        className="hover-flip-item-wrapper"
+                      >
                         <span className="hover-flip-item" itemProp="about">
                           <span data-text={metaData.cate}>{metaData.cate}</span>
                         </span>
